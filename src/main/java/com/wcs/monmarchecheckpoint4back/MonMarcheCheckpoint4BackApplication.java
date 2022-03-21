e031ca237eccb585232aa4bf164097d0b9d202db
camille <camfrelat@gmail.com>
2022-03-21 10:56:10 +0100

    initialization

---
+++ src/main/java/com/wcs/monmarchecheckpoint4back/MonMarcheCheckpoint4BackApplication.java
@@ -0,0 +1,13 @@
+package com.wcs.monmarchecheckpoint4back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonMarcheCheckpoint4BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonMarcheCheckpoint4BackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wcs/monmarchecheckpoint4back/MonMarcheCheckpoint4BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wcs.monmarchecheckpoint4back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonMarcheCheckpoint4BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
